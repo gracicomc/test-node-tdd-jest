b9395af6a9cf60141158129a4121ca5031ff31e3
Graciela Parente <graciparentte@gmail.com>
2022-05-07 18:22:49 -0300

    feat: create database

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "127.0.0.1",
+  username: "root",
+  password: "admin",
+  database: "nodeauth",
+  dialect: "mysql",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timeStamps: true, //registro de criação de tabelas
+    underscoded: true,
+    underscodedAll: true,
+  },
+};
